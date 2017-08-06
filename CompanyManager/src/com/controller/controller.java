fd896284e56a7a285177fea2c725f9f8278829ba
wangxiuqiang <wangxiuqianga@163.com>
2017-08-06 11:00:36 +0800

    企业员工管理项目

---
+++ CompanyManager/src/com/controller/controller.java
@@ -0,0 +1,11 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by wxq on 17-8-6.
+ */
+@Controller
+public class controller {
+
+}
---
+++ CompanyManager/src/com/domain/staff.java
@@ -0,0 +1,44 @@
+package com.domain;
+
+/**
+ * Created by wxq on 17-8-6.
+ */
+public class staff {
+
+    private String name;
+    private int id;
+    private String tel;
+    private int money;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
+}
