560b722a1f48a40db9d09e242eab483c763ebcb9
wangxiuqiang <wangxiuqianga@163.com>
2017-08-06 11:32:01 +0800

    企业关系系统

--- CompanyManager/src/com/controller/controller.java
+++ CompanyManager/src/com/controller/controller.java
@@ -1,6 +1,9 @@
 package com.controller;
 
+import com.domain.admin;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created by wxq on 17-8-6.
@@ -8,4 +11,13 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class controller {
 
+    @RequestMapping(value="/admin_join")
+    public String adminJoin(Model model){
+        model.addAttribute("admin",new admin());
+        return "index";
+    }
+    @RequestMapping(value = "/join_in")
+    public String In(admin admin, Model model){
+        return "CompanyShow";
+    }
 }
