0ab430139a2f6578f0c92044d89d7f78f44b13bd
wangxiuqiang <wangxiuqianga@163.com>
2017-08-10 11:44:23 +0800

    a

--- CompanyTest/src/main/java/com/service/userService.java
+++ CompanyTest/src/main/java/com/service/userService.java
@@ -2,6 +2,11 @@ package com.service;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * 用来写那个数据库的操作
  */
@@ -9,10 +14,16 @@ public class userService {
     private JdbcTemplate jdbcTemplate;
 
     public JdbcTemplate getJdbcTemplate() {
+
         return jdbcTemplate;
     }
 
     public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
+    }
+    public Map<String ,String > adminJoin(){
+        Map<String ,String> map = new HashMap<String, String>() ;
+
+        return map;
     }
 }
