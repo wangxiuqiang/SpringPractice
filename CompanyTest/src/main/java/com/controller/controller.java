1d751977d0698ed1d6f5bcda49a9b545775de95b
wangxiuqiang <wangxiuqianga@163.com>
2017-08-11 11:27:29 +0800

    1

--- CompanyTest/src/main/java/com/controller/controller.java
+++ CompanyTest/src/main/java/com/controller/controller.java
@@ -1,6 +1,7 @@
 package com.controller;
 
 import com.domain.admin;
+import com.service.userService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class controller {
 
+
     @RequestMapping(value="/admin_join")
     public String adminJoin(admin admin,Model model){
 
@@ -19,11 +21,18 @@ public class controller {
     }
     @RequestMapping(value = "/join_in")
     public String In( admin admin,Model model) {
-
-        if (admin.getName().equals("admin") && admin.getPassword().equals("admin")) {
+        userService in = new userService();
+        String[] JoinIn = in.adminJoin();
+        if(JoinIn[0].equals(null)||JoinIn[1]==null){
+            return "index";
+        }
+      else{
+          if (admin.getName().equals(JoinIn[0]) && admin.getPassword().equals(JoinIn[1])) {
             return "test";
         }else{
             return "index";
         }
+        }
+
     }
 }
--- CompanyTest/src/main/java/com/service/userService.java
+++ CompanyTest/src/main/java/com/service/userService.java
@@ -1,11 +1,10 @@
 package com.service;
 
+import com.domain.admin;
+
 import org.springframework.jdbc.core.JdbcTemplate;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 /**
  * 用来写那个数据库的操作
@@ -14,16 +13,28 @@ public class userService {
     private JdbcTemplate jdbcTemplate;
 
     public JdbcTemplate getJdbcTemplate() {
-
         return jdbcTemplate;
     }
 
     public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+
         this.jdbcTemplate = jdbcTemplate;
     }
-    public Map<String ,String > adminJoin(){
-        Map<String ,String> map = new HashMap<String, String>() ;
 
-        return map;
+    public String[] adminJoin(){
+        String[] JoinIn = new String[2];
+         //String Querysql = "select * from admin";
+        //List list =  jdbcTemplate.queryForList(Querysql);
+       // if(list.size() > 0 && list.isEmpty()){
+        //    return null;
+        //}else {
+            Iterator<admin> it = list.iterator();
+            while (it.hasNext()) {
+                admin a = (admin) it.next();
+                JoinIn[0] = a.getName();
+                JoinIn[1] = a.getPassword();
+           }
+            return JoinIn;
+        //}
     }
 }
