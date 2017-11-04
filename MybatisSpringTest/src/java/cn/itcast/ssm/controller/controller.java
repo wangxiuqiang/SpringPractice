1c6df20e7c0754cdab7c43bcd1c1f61ae3b1ed3c
wangxiuqiang <wangxiuqianga@163.com>
2017-11-04 11:21:30 +0800

    mybatis和spring整合成功

--- MybatisSpringTest/src/java/cn/itcast/ssm/controller/controller.java
+++ MybatisSpringTest/src/java/cn/itcast/ssm/controller/controller.java
@@ -1,20 +1,20 @@
 package cn.itcast.ssm.controller;
 
+
 import cn.itcast.ssm.service.impl.moviesImplClass;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class controller {
     @Autowired
-    moviesImplClass moviesImplClass;
+    public moviesImplClass implClass;
 
-    @RequestMapping( value = "query" )
+    @RequestMapping( value = "/" )
     public String query(Model model) throws Exception{
-        String name = moviesImplClass.queryInfo();
+        String name = implClass.selectName();
         model.addAttribute("name" ,name);
         return "index";
     }
--- MybatisSpringTest/src/java/cn/itcast/ssm/service/impl/moviesImplClass.java
+++ MybatisSpringTest/src/java/cn/itcast/ssm/service/impl/moviesImplClass.java
@@ -1,17 +1,17 @@
 package cn.itcast.ssm.service.impl;
 
 import cn.itcast.ssm.mapper.moviesQuery;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 @Service
-public class moviesImplClass implements moviesQuery{
+public class moviesImplClass {
+
       @Autowired
       moviesQuery moviesQuery;
 
-
-      public String queryInfo() throws Exception {
-          String name =  moviesQuery.queryInfo();
-            return name;
-      }
+    public String selectName() throws Exception{
+        String name =  moviesQuery.queryInfo();
+        return name;
+    }
 }
